15835c26c06f70350cfef3c461ec9a0510f83eb8
Pedro Henrique de V. A <pedrovasalmeida@gmail.com>
2023-01-16 21:24:49 -0300

    fix: :sparkles: add styled components theme and setup colors

---
+++ src/styles/theme.ts
@@ -0,0 +1,24 @@
+export const lightTheme = {
+  colors: {
+    background: '#F1F1F1',
+    text: '#2c2c2c',
+
+    blue_metal: {
+      300: '#9C9EB9',
+      700: '#2D3142',
+    },
+
+    green: {
+      700: '#589A5A',
+      500: '#90D692',
+    },
+
+    gray: {
+      100: '#F7F8F8',
+    },
+
+    brown: {
+      500: '#7B6F72',
+    }
+  }
+}
---
+++ src/types/styled.d.ts
@@ -0,0 +1,9 @@
+import 'styled-components'
+
+import { lightTheme as theme } from '@/styles/theme'
+
+declare module 'styled-components' {
+  type ThemeType = typeof theme
+
+  export interface DefaultTheme extends ThemeType {}
+}
