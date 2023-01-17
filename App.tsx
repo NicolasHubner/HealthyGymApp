4eb7096d9936fadf927ffae9863e584bb275b867
Pedro Henrique de V. A <pedrovasalmeida@gmail.com>
2023-01-16 21:24:25 -0300

    feat: :wrench: add react native gesture handler

--- App.tsx
+++ App.tsx
@@ -1,11 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import RoutesStack from './src/routes';
+import { Text, View } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { ThemeProvider } from "styled-components/native";
+
+import { Routes } from "@/routes";
+
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { lightTheme } from "@/styles/theme";
 
 export default function App() {
   return (
-    <>
-    <RoutesStack />
-    <StatusBar style="auto" />
-    </>
+    <SafeAreaProvider>
+      <GestureHandlerRootView style={{ flex: 1, backgroundColor: lightTheme.colors.background }}>
+        <ThemeProvider theme={lightTheme}>
+          <SafeAreaView style={{ flex: 1 }}>
+            <Routes />
+            <StatusBar style="auto" />
+          </SafeAreaView>
+        </ThemeProvider>
+      </GestureHandlerRootView>
+    </SafeAreaProvider>
   );
 }
