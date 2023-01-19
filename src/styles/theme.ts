e9fb046715535754c7aea35774a31795db2d4ce4
Pedro Henrique de V. A <pedrovasalmeida@gmail.com>
2023-01-18 22:25:31 -0300

    style: :lipstick: add new colors to theme

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,37 +1,39 @@
 export const lightTheme = {
-    font_family: {
-        regular: 'Rubik_400Regular',
-        bold: 'Rubik_700Bold',
+  font_family: {
+    regular: 'Rubik_400Regular',
+    bold: 'Rubik_700Bold',
+  },
+
+  colors: {
+    background: '#F1F1F1',
+    text: '#2c2c2c',
+
+    white: '#ffffff',
+    black: '#181818',
+
+    blue_metal: {
+      100: '#B7B7CC',
+      300: '#9C9EB9',
+      500: '#4C5980',
+      700: '#2D3142',
     },
 
-    colors: {
-        background: '#F1F1F1',
-        text: '#2c2c2c',
-
-        white: '#ffffff',
-        black: '#181818',
-
-        blue_metal: {
-            100: '#4C5980',
-            300: '#9C9EB9',
-            700: '#2D3142',
-        },
-
-        green: {
-            700: '#589A5A',
-            500: '#90D692',
-        },
-
-        gray: {
-            100: '#F7F8F8',
-            300: '#ADA4A5',
-        },
-
-        brown: {
-            500: '#7B6F72',
-        },
-        red: {
-            500: '#FF0000',
-        },
+    green: {
+      700: '#589A5A',
+      500: '#90D692',
     },
+
+    gray: {
+      100: '#F7F8F8',
+      300: '#ADA4A5',
+      200: '#B7B7CC',
+    },
+
+    brown: {
+      500: '#7B6F72',
+    },
+    red: {
+      500: '#FF0000',
+    },
+  },
 };
