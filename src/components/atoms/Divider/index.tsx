7bbae5f00b1f9515656fdc8073ebfe8a5d9bc9b8
NicolasH <hubnersantos@aol.com>
2023-01-26 14:36:23 -0300

    #FIXED: Props from DividerComponent

--- src/components/atoms/Divider/index.tsx
+++ src/components/atoms/Divider/index.tsx
@@ -3,8 +3,9 @@ import { Divider } from './style';
 
 interface IDivider {
   marginTop?: number;
+  width?: string; // Em porcentagem
 }
 
-export const DividerComponent = ({ marginTop }: IDivider) => {
-  return <Divider marginTop={marginTop} />;
+export const DividerComponent = ({ marginTop, width }: IDivider) => {
+  return <Divider width={width} marginTop={marginTop} />;
 };
--- src/components/atoms/Divider/style.ts
+++ src/components/atoms/Divider/style.ts
@@ -2,10 +2,11 @@ import styled from 'styled-components/native';
 
 interface DividerProps {
   marginTop?: number;
+  width?: string;
 }
 
 export const Divider = styled.View<DividerProps>`
-  width: 100%;
+  width: ${({ width }) => (width ? `${width}` : '100%')};
   height: 1px;
   background-color: rgba(0, 0, 0, 0.04);
   margin-top: ${({ marginTop }) => (marginTop ? `${marginTop}px` : '16px')};
