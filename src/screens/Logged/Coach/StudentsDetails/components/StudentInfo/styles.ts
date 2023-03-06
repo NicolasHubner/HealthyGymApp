be3f396c0943ff1bcc4bad4f00c5893ccfa814d2
Pedro Henrique de V. A <pedrovasalmeida@gmail.com>
2023-03-05 22:48:30 -0300

    feat: :sparkles: create observations and student details contents

---
+++ src/screens/Logged/Coach/StudentsDetails/components/StudentInfo/styles.ts
@@ -0,0 +1,54 @@
+import { baseBoldText } from '@/styles/global';
+import { scale } from 'react-native-size-matters';
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+    width: 100%;
+
+    margin-top: 32px;
+`;
+
+export const Title = styled.Text`
+    ${baseBoldText}
+    font-size: ${scale(32)}px;
+    color: ${({ theme }) => theme.colors.blue_metal[700]};
+    margin-left: 20px;
+`;
+
+export const Content = styled.View`
+    background-color: ${({ theme }) => theme.colors.white};
+    margin-top: 40px;
+    padding: 0 20px 35px;
+`;
+
+export const ContentHeader = styled.View`
+    width: 100%;
+    align-items: center;
+    justify-content: center;
+
+    padding: 40px 0 0;
+`;
+
+export const StudentImage = styled.Image.attrs({
+    width: 144,
+    height: 144,
+    resizeMode: 'cover',
+    resizeMethod: 'scale',
+})`
+    border-radius: 8px;
+`;
+
+export const StudentName = styled.Text`
+    ${baseBoldText}
+    font-size: ${scale(24)}px;
+    letter-spacing: 1px;
+
+    margin: 16px 0 8px;
+`;
+
+export const Divider = styled.View`
+    height: 0.5px;
+    width: 100%;
+
+    background-color: ${({ theme }) => theme.colors.gray[300]};
+`;
--- src/screens/Logged/Coach/StudentsDetails/index.tsx
+++ src/screens/Logged/Coach/StudentsDetails/index.tsx
@@ -1,23 +1,9 @@
 import { View } from 'react-native';
 
-import { ContentInfo } from './components/ContentInfo';
-import { StudentUsername } from '@/components/atoms/StudentUsername';
 import { ScrollablePageWrapper } from '@/components/molecules/ScreenWrapper';
 import { Header } from '@/components/organisms/Header';
 
-import {
-    Container,
-    Content,
-    ContentHeader,
-    Divider,
-    ObservationBox,
-    ObservationContent,
-    ObservationText,
-    ObservationTitle,
-    StudentImage,
-    StudentName,
-    Title,
-} from './styles';
+import { StudentInfo } from './components/StudentInfo';
 
 export function StudentsDetails() {
     return (
@@ -26,39 +12,7 @@ export function StudentsDetails() {
                 <Header />
             </View>
 
-            <Container>
-                <Title>Aluno</Title>
-
-                <Content>
-                    <ContentHeader>
-                        <StudentImage source={{ uri: 'https://fakeimg.pl/300/' }} />
-                        <StudentName>Marcelo Tavares</StudentName>
-                        <StudentUsername name="chaufinna" />
-                    </ContentHeader>
-
-                    <View style={{ paddingVertical: 24 }}>
-                        <Divider />
-                    </View>
-
-                    <ContentInfo />
-
-                    <View style={{ paddingVertical: 24 }}>
-                        <Divider />
-                    </View>
-
-                    <ObservationContent>
-                        <ObservationTitle>Observação</ObservationTitle>
-
-                        <ObservationBox>
-                            <ObservationText>
-                                Mussum Ipsum, cacilds vidis litro abertis, idolum. Admodum accumsan
-                                disputationi eu sit, outis. Vide electram sadipscing et per. Mé faiz
-                                elemun elementum girarzis, nisi eros vermeio.
-                            </ObservationText>
-                        </ObservationBox>
-                    </ObservationContent>
-                </Content>
-            </Container>
+            <StudentInfo />
         </ScrollablePageWrapper>
     );
 }
