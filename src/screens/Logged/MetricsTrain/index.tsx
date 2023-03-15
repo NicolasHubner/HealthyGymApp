688927c2bd4fcf25d03f46cc269d38070ca62716
Pedro Henrique de V. A <pedrovasalmeida@gmail.com>
2023-03-15 00:34:14 -0300

    feat: :sparkles: add api to register train

--- src/screens/Logged/MetricsTrain/index.tsx
+++ src/screens/Logged/MetricsTrain/index.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
 import { View } from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 
@@ -8,28 +9,44 @@ import { WeeklyGraph } from './components/WeeklyGraph';
 import { GraphicsList } from './components/GraphicsList';
 import { ScrollablePageWrapper } from '@/components/molecules/ScreenWrapper';
 
+import { RootState } from '@/store';
 import { throwErrorToast, throwSuccessToast } from '@/helpers/functions/handleToast';
+import { generateAuthHeaders } from '@/utils/generateAuthHeaders';
+import { api } from '@/services/api';
 
 import { GraphicContainer, InsightsButton, InsightsText } from './styles';
 
 export function MetricsTrain() {
     const [bigGraphProgress, setBigGraphProgress] = useState(0);
+    const { token, id } = useSelector((state: RootState) => state.user);
 
     const generateRandomProgress = () => {
         const random = Math.random();
         setBigGraphProgress(Number(random.toFixed(2)));
     };
 
-    const throwRandomToast = () => {
-        const success = false;
+    const handleAddTrain = async () => {
+        try {
+            const data = {
+                datetime: new Date(),
+                workout: '', // ADICIONAR O ID DO TREINO CORRETO
+                user: id,
+            };
 
-        if (success) {
+            const headers = generateAuthHeaders(token!);
+            const getTrains = await api.post('/workout-histories', data, {
+                headers,
+            });
+
+            // console.log(JSON.stringify(getTrains.data, null, 2));
+
             throwSuccessToast({
                 title: 'Treino adicionado 🤗',
                 message: 'Seu treino foi adicionado!',
                 showTime: 5000,
             });
-        } else {
+        } catch (err) {
+            console.log(err);
             throwErrorToast({
                 title: 'Ocorreu um erro 😔',
                 message: 'Houve um problema ao adicionar o treino. Por favor, tente novamente!',
@@ -49,7 +66,7 @@ export function MetricsTrain() {
             edges={['top', 'left', 'right']}
             bottomSpacing={0}>
             <View style={{ alignSelf: 'flex-end', marginRight: 24 }}>
-                <TouchableOpacity onPress={() => throwRandomToast()}>
+                <TouchableOpacity onPress={() => handleAddTrain()}>
                     <InsightsButton>
                         <InsightsText>Adicionar treino</InsightsText>
                     </InsightsButton>
