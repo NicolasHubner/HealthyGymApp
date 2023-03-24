3b13601ad999812f85d2fe190af17bfd1d37183b
Pedro Henrique de V. A <pedrovasalmeida@gmail.com>
2023-03-23 21:24:43 -0300

    fix: :bug: food screen data and sorted data

--- src/components/molecules/HeaderGoBackButton/styles.ts
+++ src/components/molecules/HeaderGoBackButton/styles.ts
@@ -4,10 +4,10 @@ export const Container = styled.View`
     align-items: center;
     justify-content: center;
 
-    width: 30px;
-    height: 30px;
+    width: 26px;
+    height: 26px;
 
+    padding-left: 4px;
     border-radius: 999px;
-
     background-color: rgba(255, 255, 255, 0.5);
 `;
