10463bec8ccbc74c086af1eeca113b86f0aae67c
Pedro Henrique de V. A <pedrovasalmeida@gmail.com>
2023-03-29 14:16:38 -0300

    fix: :bug: notification screens

--- src/screens/Logged/Notification/style.ts
+++ src/screens/Logged/Notification/style.ts
@@ -5,18 +5,17 @@ import { scale } from 'react-native-size-matters';
 export const TitleContainer = styled.View`
     width: 100%;
     justify-content: flex-start;
+    margin-bottom: 32px;
 `;
 
 export const TitleScreen = styled.Text`
     ${baseBoldText}
     font-size: ${scale(32)}px;
     color: ${({ theme }) => theme.colors.black};
-    margin-bottom: 40px;
 `;
 export const SubTitleContainer = styled.View`
     width: 100%;
     justify-content: flex-start;
-    margin-bottom: 20px;
     flex-direction: row;
 `;
 export const SubTitle = styled.Text`
@@ -42,32 +41,25 @@ export const IconText = styled.Text`
     text-align: center;
 `;
 export const ContainerNotification = styled.View`
-    width: 100%;
+    width: 95%;
     flex-direction: column;
-    /* margin-bottom: 24px; */
+    margin-bottom: 32px;
 `;
 export const NotifcationCard = styled.TouchableOpacity`
     width: 100%;
     flex-direction: row;
-    /* justify-content: flex-start; */
-    /* align-items: center; */
-    margin-bottom: 24px;
-    /* background-color: red; */
 `;
 export const CardTextContainer = styled.View`
     width: 80%;
     flex-direction: column;
     justify-content: center;
     margin-left: 20px;
-    /* align-items: flex-start; */
-    /* background-color: yellow; */
 `;
 export const CardTitle = styled.Text`
     ${baseBoldText}
     font-size: ${scale(16)}px;
-    color: ${({ theme }) => theme.colors.black};
+    color: ${({ theme }) => theme.colors.text};
     letter-spacing: 0.3px;
-    margin-bottom: 10px;
 `;
 export const CardSubTitle = styled.Text`
     ${baseRegularText}
