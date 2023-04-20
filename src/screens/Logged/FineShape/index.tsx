c2049c02e65d5d7783c35ea85a76ebc46efc2746
Pedro Henrique de V. A <pedrovasalmeida@gmail.com>
2023-04-19 22:08:42 -0300

    feat: :construction: work in progress creating fine shape flow

--- src/screens/Logged/FineShape/index.tsx
+++ src/screens/Logged/FineShape/index.tsx
@@ -1,4 +1,4 @@
-import { Text, View } from 'react-native';
+import { Pressable, Text, View } from 'react-native';
 
 import { Button } from '@/components/atoms/Button';
 
@@ -12,6 +12,9 @@ import { HeaderGoBackButton } from '@/components/molecules/HeaderGoBackButton';
 import { useDispatch, useSelector } from 'react-redux';
 import { setFineshapInfo } from '@/store/fineshape';
 import { RootState } from '@/store';
+import { useNavigation } from '@react-navigation/native';
+import { INavigation } from '@/helpers/interfaces/INavigation';
+import { RouteNames } from '@/routes/routes_names';
 
 export interface FineShapePageProps {
     title: string;
@@ -24,9 +27,10 @@ export function FineShapeBaseQuestionary() {
     const [inputValue, setInputValue] = useState('');
     const [finished, setFinished] = useState(false);
 
-    const dispatch = useDispatch();
     const { id } = useSelector((state: RootState) => state.user);
     const fineShapeStore = useSelector((state: RootState) => state.fineshape);
+    const dispatch = useDispatch();
+    const { navigate } = useNavigation<INavigation>();
 
     const maxSteps = FineShapeScreens?.length - 1 ?? 1;
 
@@ -74,6 +78,16 @@ export function FineShapeBaseQuestionary() {
                 {finished ? (
                     <View>
                         <Text>Parabéns!</Text>
+                        <Pressable onPress={() => navigate(RouteNames.logged.home)}>
+                            <Text>Voltar à home</Text>
+                        </Pressable>
+                        <Pressable
+                            onPress={() => {
+                                setCurrentStep(1);
+                                setFinished(false);
+                            }}>
+                            <Text> Voltar ao início do fine shape</Text>
+                        </Pressable>
                     </View>
                 ) : (
                     <>
