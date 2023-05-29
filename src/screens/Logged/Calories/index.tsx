1b8c6bcac2128a7117f180e607f4807da76bf0fb
Pedro Henrique <pedrovasalmeida@gmail.com>
2023-05-29 16:29:37 -0300

    feat: :sparkles: add food list on calories screen

--- src/screens/Logged/Calories/index.tsx
+++ src/screens/Logged/Calories/index.tsx
@@ -2,7 +2,9 @@ import { ScrollablePageWrapper } from '@/components/molecules/ScreenWrapper';
 import { api } from '@/services/api';
 import { RootState } from '@/store';
 import { useRoute } from '@react-navigation/native';
+import { format } from 'date-fns';
 import { useCallback, useEffect, useState } from 'react';
+import { View } from 'react-native';
 import { useSelector } from 'react-redux';
 import { IFoodDataPost } from '../Food/Daily/helpers/functions';
 import ButtonAddFoods from './ButtonAddFoods';
@@ -12,13 +14,35 @@ import {
     getTodayCaloriesConsumed,
     getTodayProteinCarboFatConsumed,
 } from './helpers/handleCalories';
-import { TopSubtitle, TopSubtitleBold, TopTitle } from './style';
+import {
+    FoodListCard,
+    FoodListCardHourWrapper,
+    FoodListCardProperty,
+    FoodListCardPropertyWrapper,
+    FoodListCardPropKey,
+    FoodListCardTitle,
+    FoodListContainer,
+    TopSubtitle,
+    TopSubtitleBold,
+    TopTitle,
+    VerticalDivider,
+} from './style';
+
+interface FoodList {
+    title: string;
+    calorie: number;
+    carbohydrate: number;
+    protein: number;
+    fat: number;
+    createdAt: string;
+}
 
 export default function Calories() {
     const { params } = useRoute() as any;
 
     const [calories, setCalories] = useState(500);
     const [buttonAdd, setButtonAdd] = useState(true);
+    const [foodList, setFoodList] = useState<FoodList[]>([]);
 
     const { goals, token, id } = useSelector((state: RootState) => state.user);
 
@@ -36,9 +60,16 @@ export default function Calories() {
         };
         try {
             const foodHistory = await api.get(
-                `/food-histories?filters[user][id][$eq]=${id}&populate=food`,
+                `/food-histories?filters[user][id][$eq]=${id}&populate=food&populate=user`,
                 { headers }
             );
+            setFoodList(
+                foodHistory?.data?.data?.map((item: any) => ({
+                    ...item?.attributes?.food?.data?.attributes,
+                    createdAt: item?.attributes?.createdAt,
+                })) ?? []
+            );
+
             setCalories(getTodayCaloriesConsumed(foodHistory.data));
 
             const { protein, carbo, fat } = getTodayProteinCarboFatConsumed(foodHistory.data);
@@ -107,6 +138,69 @@ export default function Calories() {
             <ComponentType macro={macroNutrients} total={totalMacroNutrients} />
 
             {buttonAdd && <ButtonAddFoods data={food as IFoodDataPost} />}
+
+            {foodList?.length > 0 && (
+                <FoodListContainer>
+                    <TopSubtitle>Refeições feitas hoje</TopSubtitle>
+
+                    <View style={{ width: '100%', marginTop: 16 }}>
+                        {foodList?.map((item, index) => (
+                            <FoodListCard key={item.title ?? index}>
+                                <FoodListCardTitle numberOfLines={2}>
+                                    {item.title ?? 'Vazio'}
+                                </FoodListCardTitle>
+                                <FoodListCardHourWrapper>
+                                    <FoodListCardPropKey>
+                                        {format(new Date(item.createdAt), 'dd/MM')} às{' '}
+                                        {new Date(item.createdAt).getHours()}:
+                                        {new Date(item.createdAt).getMinutes()}
+                                    </FoodListCardPropKey>
+                                </FoodListCardHourWrapper>
+                                <View
+                                    style={{
+                                        flexDirection: 'row',
+                                        justifyContent: 'space-between',
+                                        marginTop: 8,
+                                    }}>
+                                    <FoodListCardPropertyWrapper>
+                                        <FoodListCardProperty>
+                                            {item.carbohydrate ?? ''}g
+                                        </FoodListCardProperty>
+                                        <FoodListCardPropKey>Carbo</FoodListCardPropKey>
+                                    </FoodListCardPropertyWrapper>
+
+                                    <VerticalDivider />
+
+                                    <FoodListCardPropertyWrapper>
+                                        <FoodListCardProperty>
+                                            {item.fat ?? ''}g
+                                        </FoodListCardProperty>
+                                        <FoodListCardPropKey>Gordura</FoodListCardPropKey>
+                                    </FoodListCardPropertyWrapper>
+
+                                    <VerticalDivider />
+
+                                    <FoodListCardPropertyWrapper>
+                                        <FoodListCardProperty>
+                                            {item.protein ?? ''}g
+                                        </FoodListCardProperty>
+                                        <FoodListCardPropKey>Proteína</FoodListCardPropKey>
+                                    </FoodListCardPropertyWrapper>
+
+                                    <VerticalDivider />
+
+                                    <FoodListCardPropertyWrapper>
+                                        <FoodListCardProperty>
+                                            {item.calorie ?? ''}
+                                        </FoodListCardProperty>
+                                        <FoodListCardPropKey>Calorias</FoodListCardPropKey>
+                                    </FoodListCardPropertyWrapper>
+                                </View>
+                            </FoodListCard>
+                        ))}
+                    </View>
+                </FoodListContainer>
+            )}
         </ScrollablePageWrapper>
     );
 }
