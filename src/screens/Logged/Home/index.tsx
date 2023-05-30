ad08e5e3332201f3518df1fca76a4d17f5ff630b
NicolasH <hubnersantos@aol.com>
2023-05-30 10:20:22 -0300

    #FIX Home

--- src/screens/Logged/Home/index.tsx
+++ src/screens/Logged/Home/index.tsx
@@ -13,6 +13,7 @@ import { RootState } from '@/store';
 import { OptionsContainer, TitleNavigationApp, TitleNavigationContainer } from './styles';
 import { generateAuthHeaders } from '@/utils/generateAuthHeaders';
 import { sentPhotos } from './helpers/sentPhotos';
+import { verticalScale } from 'react-native-size-matters';
 
 const cardWarningsPattern = {
     user: {
@@ -62,7 +63,7 @@ export function Home() {
                 <TitleNavigationApp>Navegue pelo seu app</TitleNavigationApp>
             </TitleNavigationContainer>
 
-            <OptionsContainer style={{ rowGap: 12 }}>
+            <OptionsContainer style={{ rowGap: verticalScale(16) }}>
                 {userRole === 'coach' ? <HomeOptionsForCoach /> : <HomeOptionsForNormalUser />}
             </OptionsContainer>
         </ScrollablePageWrapper>
