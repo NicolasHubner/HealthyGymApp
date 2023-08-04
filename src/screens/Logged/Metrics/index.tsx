2baa12b01920109b4ad169eebaf37c0ffdcacc0e
NicolasH <hubnersantos@aol.com>
2023-08-04 15:53:44 -0300

    fix: :bug: Header Metrics

--- src/screens/Logged/Metrics/index.tsx
+++ src/screens/Logged/Metrics/index.tsx
@@ -1,14 +1,18 @@
 import TitlePattern from '@/components/atoms/TitlePattern';
 import { ScrollablePageWrapper } from '@/components/molecules/ScreenWrapper';
 import { CardTitle } from './style';
-import { FontAwesome5 } from '@expo/vector-icons';
+// import { FontAwesome5 } from '@expo/vector-icons';
 import { Header } from '@/components/organisms/Header';
 import { MetricsInfographic } from '@/components/organisms/MetricsInfographic';
+import { RootState } from '@/store';
+import { useSelector } from 'react-redux';
 
 export default function Metrics() {
+    const { imageProfile } = useSelector((state: RootState) => state.user);
+
     return (
         <ScrollablePageWrapper padding={16} bottomSpacing={40}>
-            <Header />
+            <Header imageProfile={imageProfile || ''} />
 
             <CardTitle>
                 <TitlePattern size={20}>Métricas</TitlePattern>
